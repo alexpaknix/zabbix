81aafc6e09810e56f41ac0391352e8c022bb5d89
Aleksandr <pakhomov@X57516109.ds.local>
2013-08-02 14:27:27 +0400

    first commit

---
+++ opt/zabbix/sh/PSAPBWn702_tablespace.sh
@@ -0,0 +1,5 @@
+#!/sbin/sh
+. /opt/zabbix/oraenv/BWn
+sql="set pagesize 0
+select tablespace_name,round(TABLESPACE_SIZE*8/1024-USED_SPACE*8/1024) from dba_tablespace_usage_metrics;"
+echo "$sql" | sqlplus -s /nolog @/opt/zabbix/conn.sql | awk '/PSAPBWn702/ {print $2}'
