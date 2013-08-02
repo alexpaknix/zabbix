c28eb11f161002125a45dfd086eef6b19b4047ca
Alexander <alexpaknix@gmail.com>
2013-08-01 23:46:11 -0700

    Update root.sh

--- opt/zabbix/root.sh
+++ opt/zabbix/root.sh
@@ -1,17 +1,16 @@
 #!/sbin/sh
 case $1 in
 	"check")
-	echo "Check Zabbix files"
+	echo "Check Zabbix files - none"
 	;;
 	
-	* )
-	echo 'Check usage:' $0 'check\nFor check files'
+	"install")
+	#echo 'Check usage:' $0 'check\nFor check files'
 	chown -R zabbix:users /opt/zabbix
-	chmod +x /opt/zabbix/sh/*.sh
-	chmod +x /opt/zabbix/sbin/*
-	chmod +x /opt/zabbix/bin/*
-	rm -rf /opt/zabbix/tmp/*
+	chmod -R 744 /opt/zabbix/sh
+	chmod +x /opt/zabbix/{sbin,bin}/*
+	#rm -rf /opt/zabbix/tmp/*
 	chmod +x /sbin/init.d/zabbix
-	#ln -s /sbin/init.d/zabbix /sbin/rc3.d/S999zabbix
+	ln -s /sbin/init.d/zabbix /sbin/rc3.d/S999zabbix
 	;;
 esac
