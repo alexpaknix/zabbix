3dead11acfb7a16dfa1fd90de419725b2b62937c
Alexander <alexpaknix@gmail.com>
2014-02-19 09:44:16 +0300

    Update conn.sql

--- opt/zabbix/conn.sql
+++ opt/zabbix/conn.sql
@@ -2,3 +2,4 @@ whenever sqlerror exit failure
 set verify off echo off feedback off heading off pagesize 0 trimout on trimspool on termout off 
 conn user/password
 column retvalue format a15
+set linesize 300
