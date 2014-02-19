2c5f01a9a6b4d1f0c2856993aae943cc8580e679
Alexander <alexpaknix@gmail.com>
2014-02-19 10:27:16 +0300

    Update PSAPBWn_tablespace.sh

--- opt/zabbix/sh/PSAPBWn_tablespace.sh
+++ opt/zabbix/sh/PSAPBWn_tablespace.sh
@@ -1,5 +1,6 @@
 #!/sbin/sh
 . /opt/zabbix/oraenv/BWn
 sql="set pagesize 0
-select tablespace_name,round(TABLESPACE_SIZE*8/1024-USED_SPACE*8/1024) from dba_tablespace_usage_metrics;"
-echo "$sql" | sqlplus -s /nolog @/opt/zabbix/conn.sql | awk '/PSAPBWn\ / {print $2}'
+set numwidth 18;
+select tablespace_name,round((TABLESPACE_SIZE*8)*1024-(USED_SPACE*8)*1024) from dba_tablespace_usage_metrics;"
+echo "$sql" | sqlplus -s /nolog @/opt/zabbix/conn.sql | awk '/PSAPSID\ / {print $2}'
