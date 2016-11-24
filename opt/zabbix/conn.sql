0a2ab2a03230f0f28f3096df613b59f1b0ce28eb
Alexander <alexpaknix@gmail.com>
2016-11-24 11:43:05 +0300

    Delete conn.sql

--- opt/zabbix/conn.sql
+++
@@ -1,5 +0,0 @@
-whenever sqlerror exit failure 
-set verify off echo off feedback off heading off pagesize 0 trimout on trimspool on termout off 
-conn user/password
-column retvalue format a15
-set linesize 300
